200a4c2072e544473803f7aadc2fa8e637b5c90f
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-07-31 19:59:53 -0300

    Data now stored as instance attribute

--- dpclient.py
+++ dpclient.py
@@ -16,16 +16,17 @@ INITIAL_DATA = {
 class DpClient(object):
     def __init__(self, data_file):
         self.data_file = data_file
+        self._read_data()
 
     def _read_data(self):
         '''Read stored data.'''
         if not os.path.exists(self.data_file):
-            self._save_data(INITIAL_DATA)
+            self.data = INITIAL_DATA
+            self._save_data()
         json_data = json.loads(open(self.data_file).read())
-        return bunchify(json_data)
+        self.data = bunchify(json_data)
 
-    def _save_data(self, data):
+    def _save_data(self):
         '''Save data.'''
         with open(self.data_file, 'w') as f:
-            f.write(json.dumps(data))
-
+            f.write(json.dumps(self.data))
