723a9a825b49b540e71ecd0eaae57afd8e2b3386
Juan Pedro Fisanotti <fisadev@gmail.com>
2012-07-31 19:54:04 -0300

    Created a class to contain the dpclient

--- dpclient.py
+++ dpclient.py
@@ -1,9 +1,10 @@
 import os
+import sys
 import json
 from bunch import bunchify
 from browser import DotProjectBot
 
-DATA_FILE = os.path.expanduser('~/.dpclient')
+DEFAULT_DATA_FILE = os.path.expanduser('~/.dpclient')
 INITIAL_DATA = {
         'user': '',
         'password': '',
@@ -12,15 +13,19 @@ INITIAL_DATA = {
 }
 
 
-def read_data():
-    '''Read stored data.'''
-    if not os.path.exists(DATA_FILE):
-        save_data(INITIAL_DATA)
-    json_data = json.loads(open(DATA_FILE).read())
-    return bunchify(json_data)
+class DpClient(object):
+    def __init__(self, data_file):
+        self.data_file = data_file
 
+    def read_data(self):
+        '''Read stored data.'''
+        if not os.path.exists(self.data_file):
+            self.save_data(INITIAL_DATA)
+        json_data = json.loads(open(self.data_file).read())
+        return bunchify(json_data)
 
-def save_data(data):
-    '''Save data.'''
-    with open(DATA_FILE, 'w') as f:
-        f.write(json.dumps(data))
+    def save_data(self, data):
+        '''Save data.'''
+        with open(self.data_file, 'w') as f:
+            f.write(json.dumps(data))
+
